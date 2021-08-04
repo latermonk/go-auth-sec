8617596d82f9006d376078ae08cd92244f97b8af
latermonk <iback.china@gmail.com>
2021-08-04 12:57:56 +0800

    Initial commit

---
+++ OAuth2/go.mod
@@ -0,0 +1,8 @@
+module OAuth2
+
+go 1.16
+
+require (
+	github.com/agtorre/go-cookbook v0.0.0-20180611154932-b631e0da3cdd
+	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914
+)
---
+++ OAuth2/oauthcli/config.go
@@ -0,0 +1,37 @@
+package oauthcli
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
+)
+
+// 设置oauth2.Config
+// 需要在环境变量中设置GITHUB_CLIENT GITHUB_SECRET
+func Setup() *oauth2.Config {
+	return &oauth2.Config{
+		ClientID:     os.Getenv("GITHUB_CLIENT"),
+		ClientSecret: os.Getenv("GITHUB_SECRET"),
+		Scopes:       []string{"repo", "user"},
+		Endpoint:     github.Endpoint,
+	}
+}
+
+// GetToken 检索github oauth2令牌
+func GetToken(ctx context.Context, conf *oauth2.Config) (*oauth2.Token, error) {
+	url := conf.AuthCodeURL("state")
+	fmt.Printf("Type the following url into your browser and follow the directions on screen: %v\n", url)
+	fmt.Println("Paste the code returned in the redirect URL and hit Enter:")
+
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		return nil, err
+	}
+	return conf.Exchange(ctx, code)
+}
+
+
+
